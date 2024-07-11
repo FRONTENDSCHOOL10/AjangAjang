dfddcdda98f3d72515e3d63aab9ff0b10245451d
siyo0723 <junior0723@naver.com>
2024-07-11 15:26:06 +0900

    상세페이지 컴포넌트 scss 수정 마무리

--- src/components/detail-count.js
+++ src/components/detail-count.js
@@ -1,27 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const counterDisplay = document.getElementById('counter');
-  const totalPriceDisplay = document.getElementById('total-price');
-  const increaseButton = document.getElementById('increase');
-  const decreaseButton = document.getElementById('decrease');
+document.addEventListener("DOMContentLoaded", function () {
+  const counterDisplay = document.getElementById("counter");
+  const totalPriceDisplay = document.getElementById("total-price");
+  const increaseButton = document.getElementById("increase");
+  const decreaseButton = document.getElementById("decrease");
   const pricePerItem = 5000;
   let counter = 0;
 
   function updateTotalPrice() {
-      const totalPrice = counter * pricePerItem;
-      totalPriceDisplay.textContent = totalPrice;
+    const totalPrice = counter * pricePerItem;
+    totalPriceDisplay.textContent = totalPrice;
   }
 
-  increaseButton.addEventListener('click', function() {
-      counter++;
-      counterDisplay.textContent = counter;
-      updateTotalPrice();
+  increaseButton.addEventListener("click", function () {
+    counter++;
+    counterDisplay.textContent = counter;
+    updateTotalPrice();
   });
 
-  decreaseButton.addEventListener('click', function() {
-      if (counter > 0) {
-          counter--;
-          counterDisplay.textContent = counter;
-          updateTotalPrice();
-      }
+  decreaseButton.addEventListener("click", function () {
+    if (counter > 0) {
+      counter--;
+      counterDisplay.textContent = counter;
+      updateTotalPrice();
+    }
   });
 });
