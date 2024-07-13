8e79aee56e893ebb61724d8efafa98b419156a89
otwaylee <ahyeong858@gmail.com>
2024-07-14 02:48:38 +0900

    add: 포켓베이스 상품 데이터 추가 (18개)

---
+++ pocketbase/pb_migrations/1720891734_updated_products.js
@@ -0,0 +1,136 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("8hlnndlejszxifv")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "pq8p0o3n",
+    "name": "description",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "rs1xmdge",
+    "name": "price",
+    "type": "number",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "noDecimal": false
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "fyulp2dg",
+    "name": "unit",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "cucjzbbj",
+    "name": "allergy",
+    "type": "editor",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "convertUrls": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("8hlnndlejszxifv")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "pq8p0o3n",
+    "name": "description",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "rs1xmdge",
+    "name": "cost",
+    "type": "number",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "noDecimal": false
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "fyulp2dg",
+    "name": "unit",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "cucjzbbj",
+    "name": "allergy",
+    "type": "editor",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "convertUrls": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
