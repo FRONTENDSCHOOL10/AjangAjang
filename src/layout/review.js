b386ced7b96d6d1959ee764ff38951dff777c1e9
juwon <juwon0481@gmail.com>
2024-07-15 16:06:56 +0900

    modify: 리뷰 데이터 출력 js 수정

--- src/layout/review.js
+++ src/layout/review.js
@@ -1,46 +1,50 @@
 import "@/sass/main.scss";
-import gsap from "gsap";
-import { comma, insertLast, setDocumentTitle, getStorage, setStorage } from "kind-tiger";
+//import gsap from "gsap";
+import { insertLast, setDocumentTitle } from "kind-tiger";
 import pb from "@/api/pocketbase";
-import getPbImageURL from "@/api/getPbImageURL";
-import defaultAuthData from "@/api/defaultAuthData";
 
 setDocumentTitle("컬리 / 상품리뷰");
 
-if (!localStorage.getItem("auth")) {
-  setStorage("auth", defaultAuthData);
-}
-
-async function renderProductItem() {
+async function renderReviewItem() {
   const reviewData = await pb.collection("reviews").getFullList({
     sort: "-created",
+    expand: "review_user,review_product",
   });
-
-  //const response = await tiger.get(`${import.meta.env.VITE_PB_API}/collections/products/records`);
-  //const productsData = response.data.items;
-  console.log(reviewData);
-
-  const { isAuth } = await getStorage("auth");
 
   reviewData.forEach((item) => {
-    const template = `
-    <div class="review-member-list">
-    <ul class="review-member">
-    <li class="badge best-badge">베스트</li>
-    <li class="badge class-badge">퍼플</li>
-    <li class="user-name">${item.review_user}</li>
-  </ul>
-  <ul class="review-product">
-    <li class="review-product-name">${item.review_product}</li>
-    <li class="review-text">${item.review_text}</li>
-    <li class="review-write-data">${item.updated}</li>
-  </ul>
-     </div>
-    `;
-    insertLast(".review-wrap", template);
-  });
+    const reviewUser = item.expand.review_user.name;
+    const reviewProduct = item.expand.review_product.title;
+    const maskName = reviewUser.slice(0, 1) + "*".repeat(reviewUser.length - 2) + reviewUser.slice(-1);
 
-  gsap.from(".review-wrap", { y: 30, opacity: 0, stagger: 0.1 });
+    if (item.notice) {
+      const template2 = `
+      <details>
+      <summary><span class="notice-badge">공지</span> ${item.review_title}</summary>
+      <div class="review-notice">
+        ${item.review_text}
+      </div>
+    </details>
+      `;
+      insertLast(".review-list", template2);
+    } else {
+      const dete = item.updated.slice(0, 10);
+      const template = `
+      <div class="review-member-list">
+      <ul class="review-member">
+      <li class="badge best-badge">베스트</li>
+      <li class="badge class-badge">퍼플</li>
+      <li class="user-name">${maskName}</li>
+    </ul>
+    <ul class="review-product">
+      <li class="review-product-name">${reviewProduct}</li>
+      <li class="review-text">${item.review_text}</li>
+      <li class="review-write-data">${dete}</li>
+    </ul>
+       </div>
+      `;
+      insertLast(".review-wrap", template);
+    }
+  });
 }
 
-renderProductItem();
+renderReviewItem();
