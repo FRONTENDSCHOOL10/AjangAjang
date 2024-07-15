e77c36ba8baf4bdd65a787a1e9a5d0224e40c2b0
otwaylee <ahyeong858@gmail.com>
2024-07-15 12:37:09 +0900

    modify: 경로 수정

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-import "/src/components/swiper-script.js";
-import "/src/pages/login/login"
-import "/src/pages/products/products.js";
 import "/src/sass/main.scss";
+import "/src/components/swiper-script.js";
+// import "/src/pages/products/products.js";
