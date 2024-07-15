08faf8affd784f3e1c4ae0f98f4bcbc3c73c26b6
otwaylee <ahyeong858@gmail.com>
2024-07-15 15:57:34 +0900

    modify: 체크박스 컴포넌트 수정 및 js 추가

---
+++ src/components/input-checkbox.js
@@ -0,0 +1,22 @@
+
+  const agreeAllCheckbox = document.getElementById('agreeAll');
+  const checkboxes = document.querySelectorAll('.agreement-option input[type="checkbox"]:not(#agreeAll)');
+
+  agreeAllCheckbox.addEventListener('change', function () {
+    checkboxes.forEach(function (checkbox) {
+      checkbox.checked = agreeAllCheckbox.checked;
+    });
+  });
+
+  checkboxes.forEach(function (checkbox) {
+    checkbox.addEventListener('change', function () {
+      if (!checkbox.checked) {
+        agreeAllCheckbox.checked = false;
+      } else {
+        const allChecked = Array.from(checkboxes).every(function (checkbox) { 
+          return checkbox.checked;
+        });
+        agreeAllCheckbox.checked = allChecked;
+      }
+    });
+  });
