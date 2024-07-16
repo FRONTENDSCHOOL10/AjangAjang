e54234c59835fcc65488bb8c4c55acca9380e730
otwaylee <ahyeong858@gmail.com>
2024-07-16 17:43:27 +0900

    comment: main.js 주석 취소

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import "/src/components/swiper-script.js";
-//import "/src/components/product-card.js";
-import "/src/components/footer.js";
-import "/src/sass/main.scss";
+import '/src/components/swiper-script.js';
+import '/src/components/product-card.js';
+import '/src/components/footer.js';
+import '/src/sass/main.scss';
