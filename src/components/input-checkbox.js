8bd86326c41543369fb86afed5045f99e9c25de0
otwaylee <ahyeong858@gmail.com>
2024-07-16 14:24:49 +0900

    modify: 회원가입 페이지 js 구현 - 체크박스 전체 체크 기능 - 이메일 중복확인 - 포켓베이스에 데이터 전송

--- src/components/input-checkbox.js
+++ src/components/input-checkbox.js
@@ -1,22 +1,23 @@
+const agreeAllCheckbox = document.getElementById('agreeAll');
+const checkboxes = document.querySelectorAll(
+  '.agreement-option input[type="checkbox"]:not(#agreeAll)'
+);
 
-  const agreeAllCheckbox = document.getElementById('agreeAll');
-  const checkboxes = document.querySelectorAll('.agreement-option input[type="checkbox"]:not(#agreeAll)');
-
-  agreeAllCheckbox.addEventListener('change', function () {
-    checkboxes.forEach(function (checkbox) {
-      checkbox.checked = agreeAllCheckbox.checked;
-    });
-  });
-
+agreeAllCheckbox.addEventListener('change', function () {
   checkboxes.forEach(function (checkbox) {
-    checkbox.addEventListener('change', function () {
-      if (!checkbox.checked) {
-        agreeAllCheckbox.checked = false;
-      } else {
-        const allChecked = Array.from(checkboxes).every(function (checkbox) { 
-          return checkbox.checked;
-        });
-        agreeAllCheckbox.checked = allChecked;
-      }
-    });
+    checkbox.checked = agreeAllCheckbox.checked;
   });
+});
+
+checkboxes.forEach(function (checkbox) {
+  checkbox.addEventListener('change', function () {
+    if (!checkbox.checked) {
+      agreeAllCheckbox.checked = false;
+    } else {
+      const allChecked = Array.from(checkboxes).every(function (checkbox) {
+        return checkbox.checked;
+      });
+      agreeAllCheckbox.checked = allChecked;
+    }
+  });
+});
