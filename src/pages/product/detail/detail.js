cb00a668ce1e0332488ee09ab031d2241a9e677b
juwon <juwon0481@gmail.com>
2024-07-18 21:50:11 +0900

    modify: 디테일페이지 파일 수정

--- src/pages/product/detail/detail.js
+++ src/pages/product/detail/detail.js
@@ -1,6 +1,6 @@
 import "/src/components/header.js";
 import "/src/pages/product/detail/detailView.js";
-import "/src/pages/product/detail/reviewView.js";
+//import "/src/pages/product/detail/reviewView.js";
 // import "/src/pages/product/detail/reviewWrite.js";
 // import "/src/pages/product/detail/inquiryView.js";
 // import "/src/pages/product/detail/inquiryWrite.js";
